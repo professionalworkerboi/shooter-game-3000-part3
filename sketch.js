45f155c913e415909d57dd77db8548c3ca0fb8d6
bob <darshard1234@gmail.com>
2021-08-09 18:08:00 +0530

    test

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
+var shooter,asteroids,rareAsteroid;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(windowWidth-5,windowHeight-5);
+  shooter = createSprite(displayWidth/2,displayHeight/1.35, 50, 50);
+
 }
 
 function draw() {
   background(255,255,255);  
+  
+  if(keyDown(UP_ARROW)){
+    shooter.y = shooter.y - 10;
+  }
+  
+  if(keyDown(DOWN_ARROW)){
+    shooter.y = shooter.y + 10;
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+    shooter.x = shooter.x + 10;
+  }
+
+  if(keyDown(LEFT_ARROW)){
+    shooter.x = shooter.x - 10;
+  }
+
+  if(keyDown("space")){
+    Laser();
+    laser.velocityY =  - 5;
+  }
   drawSprites();
+  
 }
+
+function Laser(){
+  laser = createSprite(displayWidth/2,displayHeight/1.35,50,50);
+  laser.x = shooter.x;
+  laser.y = shooter.y;
+} 
