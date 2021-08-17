8876591a21925cc92a760c45d99787c496dbf396
bob <darshard1234@gmail.com>
2021-08-17 17:57:16 +0530

    t

--- sketch.js
+++ sketch.js
@@ -1,40 +1,69 @@
 var shooter,asteroids,rareAsteroid;
 
+var gameState = "start";
+
+function preload(){
+  bgImg = loadImage("images/space v2.jpg");
+}
+
 function setup() {
   createCanvas(windowWidth-5,windowHeight-5);
   shooter = createSprite(displayWidth/2,displayHeight/1.35, 50, 50);
   shooter.shapeColor = "green";
   astGroup = new Group();
   planetGroup = new Group();
-  starGroup = new Groups();
+  starGroup = new Group();
+  laserGroup = new Group();
 }
 
 function draw() {
-  background(255,255,255);  
-  asteroids();
-  planets();
-  stars();
-  if(keyDown(UP_ARROW)){
-    shooter.y = shooter.y - 30;
+  background(bgImg);
+  if(gameState === "start"){
+    textSize(50);
+    fill("white");
+    text("With the help of 'S' start the game",displayWidth/3.5,displayHeight/3.5);
+    shooter.visible = false; 
+    if(keyCode === 83){
+      gameState = "play";
+    }
   }
-  
-  if(keyDown(DOWN_ARROW)){
-    shooter.y = shooter.y + 30;
-  }
+  else if(gameState === "play"){
+    shooter.visible = true;
+    asteroids();
+    planets();
+    stars();
+    if(keyDown(UP_ARROW)){
+      shooter.y = shooter.y - 30;
+    }
+    
+    if(keyDown(DOWN_ARROW)){
+      shooter.y = shooter.y + 30;
+    }
 
-  if(keyDown(RIGHT_ARROW)){
-    shooter.x = shooter.x + 30;
-  }
+    if(keyDown(RIGHT_ARROW)){
+      shooter.x = shooter.x + 30;
+    }
 
-  if(keyDown(LEFT_ARROW)){
-    shooter.x = shooter.x - 30;
-  }
+    if(keyDown(LEFT_ARROW)){
+      shooter.x = shooter.x - 30;
+    }
 
-  if(keyDown("space")){
-    Laser();
-    laser.velocityY =  - 5;
+    if(keyDown("space")){
+      Laser();
+      laser.velocityY =  - 5;
+    }
+    if(astGroup.isTouching(laserGroup)){
+      for(var i = 0; i < astGroup.lenght; i++){
+        if(astGroup[i].isTouching(laserGroup)){
+          astGroup[i].destroy();
+          laserGroup.destroyEach();
+        }
+      }
+
+    }
   }
   drawSprites();
+  
   
 }
 
@@ -46,6 +75,7 @@ function Laser(){
   laser.depth = shooter.depth;
   shooter.depth += 1;
   laser.lifetime = 200;
+  laserGroup.add(laser);
 } 
 
 function asteroids(){
@@ -80,3 +110,4 @@ if(frameCount%100===0){
   planetGroup.add(planet);
 }
 }
+
